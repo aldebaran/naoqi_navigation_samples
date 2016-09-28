bc66c8fe3d73a9b75d67e86d3178bfeb292d2259
lsouchet <lsouchet@aldebaran-robotics.com>
2016-09-28 16:03:43 +0200

    EXPLORE: version 0.0.1 OK.
    Change-Id: I8a6c67bacaa998ec8f4be9641fc394b92b6e6baa
    Reviewed-on: http://gerrit.aldebaran.lan/71865
    Reviewed-by: ngarcia <ngarcia@aldebaran-robotics.com>
    Tested-by: ngarcia <ngarcia@aldebaran-robotics.com>

---
+++ explore/explore.pml
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="explore" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="explore_display" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="explore_move" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="all" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="index" src="html/index.html" />
+        <File name="jquery-1.11.0.min" src="html/js/jquery-1.11.0.min.js" />
+        <File name="main" src="html/js/main.js" />
+        <File name="robotutils" src="html/js/robotutils.js" />
+        <File name="icon" src="html/img/icon.png" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ explore/translations/translation_en_US.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Prouto des</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Prouto des</translation>
+        </message>
+        <message>
+            <source>prouto des</source>
+            <comment>Text</comment>
+            <translation type="obsolete">prouto des</translation>
+        </message>
+        <message>
+            <source>prouto des prouto des prouto des prouto des prouto des prouto des</source>
+            <comment>Text</comment>
+            <translation type="obsolete">prouto des prouto des prouto des prouto des prouto des prouto des</translation>
+        </message>
+        <message>
+            <source>prouto des prout</source>
+            <comment>Text</comment>
+            <translation type="obsolete">prouto des prout</translation>
+        </message>
+    </context>
+</TS>
